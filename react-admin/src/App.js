7cf3d61040e0ec4b39872acc42d0923acac37307
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-10 17:09:37 +0530

    create posts editor

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import UserList from './components/userList';
 import PostList from './components/postList';
+import PostEdit from './components/postEdit';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
@@ -11,8 +12,8 @@ const App = () => (
   <Admin dataProvider={dataProvider}>
     {/* <Resource name="users" list={ListGuesser} /> */}
     {/* <Resource name="posts" list={ListGuesser} /> */}
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
     <Resource name="users" list={UserList}/>
-    <Resource name="posts" list={PostList} />
   </Admin>
 
 );
---
+++ react-admin/src/components/postEdit.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Edit, SimpleForm, ReferenceInput, TextInput, SelectInput } from 'react-admin';
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="id" /></ReferenceInput>
+            <TextInput source="id" />
+            <TextInput source="title" />
+            TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export default PostEdit;
