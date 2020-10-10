fde5c6eb0f0a0d07514f0f3b258c8e92f57e83cd
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-10 17:29:16 +0530

    add icons

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -2,6 +2,9 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
 import UserList from './components/userList';
 import PostList from './components/postList';
 import PostEdit from './components/postEdit';
@@ -13,8 +16,8 @@ const App = () => (
   <Admin dataProvider={dataProvider}>
     {/* <Resource name="users" list={ListGuesser} /> */}
     {/* <Resource name="posts" list={ListGuesser} /> */}
-    <Resource name="users" list={UserList}/>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+    <Resource name="users" list={UserList} icon={UserIcon}/>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
   </Admin>
 
 );
