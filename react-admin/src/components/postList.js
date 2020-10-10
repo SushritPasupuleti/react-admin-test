93637e6ffac20fb7e429957a28b4bf2215fc1b69
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-10 17:26:08 +0530

    add search to posts view

--- react-admin/src/components/postList.js
+++ react-admin/src/components/postList.js
@@ -1,8 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import { Filter, ReferenceInput, SelectInput, TextInput, List, TextField, ReferenceField, Datagrid, EditButton } from 'react-admin';
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
 
 const PostList = props => (
-    <List {...props}>
+    <List filters={<PostFilter />} {...props}>
         <Datagrid>
            <TextField source="id" />
             <ReferenceField source="userId" reference="users">
