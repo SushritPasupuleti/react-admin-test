c9f7932d10e628d73a88f07bd916912d445bf590
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-10 17:19:29 +0530

    add title based header in post editor

--- react-admin/src/components/postEdit.js
+++ react-admin/src/components/postEdit.js
@@ -1,8 +1,12 @@
 import * as React from "react";
 import { Edit, SimpleForm, ReferenceInput, TextInput, SelectInput } from 'react-admin';
 
+export const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostEdit = props => (
-    <Edit {...props}>
+    <Edit title={<PostTitle/>} {...props}>
         <SimpleForm>
             <TextInput disabled source="id" />
             <ReferenceInput source="userId" reference="users"><SelectInput optionText="name" /></ReferenceInput>
