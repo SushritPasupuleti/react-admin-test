6aa6192a01a023e0b7142a4e8754d9babeed01e1
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-01-10 18:25:27 +0530

    Update App.js

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -44,7 +44,7 @@ const App = () => {
       {/* // <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}> */}
       {/* <Resource name="users" list={ListGuesser} /> */}
       {/* <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/> */}
-      {/* <Resource name="users" list={UserList} icon={UserIcon}/> */}
+      <Resource name="users" list={UserList} icon={UserIcon}/>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     </Admin>
 
