ff09c22f450bc268fd92ca15d7d52cf6f6eaece2
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-01-10 18:25:30 +0530

    Update userList.js

--- react-admin/src/components/userList.js
+++ react-admin/src/components/userList.js
@@ -8,10 +8,7 @@ const UserList = props => (
             <TextField source="name" />
             <TextField source="username" />
             <EmailField source="email" />
-            <TextField source="address.street" />
             <TextField source="phone" />
-            <TextField source="website" />
-            <TextField source="company.name" />
         </Datagrid>
     </List>
 );
